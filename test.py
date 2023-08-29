c245685286a39fc7cddbd60be2eda2bc3f220329
Hexiang Qi <hexiang@hexiang-zbook>
2023-08-29 23:48:45 +0800

    Clamp check

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
-from workflow.Workflow import Workflow
+from typing import Any
+from layers.StaticLayer import StaticLayer
+from layers.UtilLayers import AutoBondLayer
+from workflow.Workflow import Workflow, default_runners
 from fs.osfs import OSFS
 import yaml
+from libs.RadiiTable import default_radius_table
 
 workflow_data = yaml.load(OSFS("./").readtext("test.yml"), yaml.Loader)
 
-workflow = Workflow(workflow_data)
+class ClampRisky:
+    def __init__(self, options, meta):
+        self.radius_table = {
+            key: default_radius_table[key] for key in default_radius_table
+        }
+    
+    def __call__(self, item, tags):
+        re_connected = StaticLayer(contains=(item, [AutoBondLayer()]))
+        if len(re_connected.bond_ids) != len(item.bond_ids):
+            print(f"risky: tags: {tags}")
+        return item, ""
+
+workflow = Workflow(workflow_data, runners=default_runners | {"risky": (ClampRisky, False)})
 
 workflow.run()
